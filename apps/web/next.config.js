f678fc9a44c00416fa49ec95172aa3ba355ffb65
RuHd <ruaan.ram@gmail.com>
2026-04-23 14:03:13 -0300

    feat: Design Tokens Implementation

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    allowedDevOrigins: ['192.168.125.130:3000', 'localhost:3000', "192.168.152.130:3000"]
+};
 
 export default nextConfig;
